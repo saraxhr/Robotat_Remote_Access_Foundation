ad35ec6a0459f97914ecd8b47169e284a38f34ac
saraxhr <her21743@uvg.edu.gt>
2026-07-07 13:15:53 -0600

    Update config.py

--- pagina_web/backend/camaras/pagina_web/backend/camaras/flask_video_server/config.py
+++ pagina_web/backend/camaras/pagina_web/backend/camaras/flask_video_server/config.py
@@ -6,17 +6,16 @@
 
 CAMERAS = {
     "1": {  # ID lógico que usará el frontend (cámara 1)
-        "ip": "192.168.50.211",                                   # IP de la cámara
-        "user": "admin",                                          # Usuario de la cámara
-        "password": "UVG12345678",                                # Contraseña de la cámara
-        "rtsp": "rtsp://admin:UVG12345678@192.168.50.211:554/cam/realmonitor?channel=1&subtype=1",  # RTSP (substream)
+        "ip": "",                                   # IP de la cámara
+        "user": "",                                          # Usuario de la cámara
+        "password": "",                                # Contraseña de la cámara
+        "rtsp": "rtsp://user:password@IP:554/cam/realmonitor?channel=1&subtype=1",  # RTSP (substream)
     },
     "2": {  # ID lógico que usará el frontend (cámara 2)
-        "ip": "192.168.50.212",                                    # IP de la cámara
-        #"ip": "192.168.1.36",
-        "user": "admin",                                          # Usuario de la cámara
-        "password": "12345678UVG",                                # Contraseña de la cámara
-        "rtsp": "rtsp://admin:12345678UVG@192.168.50.212:554/cam/realmonitor?channel=1&subtype=1",  # RTSP (substream)
+        "ip": "",                                    # IP de la cámara
+        "user": "",                                          # Usuario de la cámara
+        "password": "",                                # Contraseña de la cámara
+       "rtsp": "rtsp://user:password@IP:554/cam/realmonitor?channel=1&subtype=1",  # RTSP (substream)
 
 
     },
